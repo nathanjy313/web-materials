a2dfebab1a74f669351a05481fdcafdc92446cf9
nathanjy313 <nathanjy@uw.edu>
2018-05-18 08:16:48 -0700

    participation 8

--- exercise-1/ui.R
+++ exercise-1/ui.R
@@ -1,7 +1,9 @@
 # ui.R
+#install.packages("shinythemes")
 library(shiny)
 library(plotly)
-shinyUI(navbarPage(
+library(shinythemes)
+shinyUI(navbarPage(theme = shinytheme("sandstone"),
   "Electoral College",
   # Create a tab panel for your map
   tabPanel(
@@ -12,6 +14,7 @@ shinyUI(navbarPage(
 
       # Side panel for controls
       sidebarPanel(
+        tags$h4("This is a really cool interactive map"),
 
         # Input to select variable to map
         selectInput(
@@ -24,12 +27,24 @@ shinyUI(navbarPage(
           )
         )
       ),
-
+      
       # Main panel: display plotly map
       mainPanel(
-        plotlyOutput("map")
+        plotlyOutput("map"),
+        tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+        tags$div(class = "summary", checked = NA, 
+        tags$p("This map shows the electoral college votes by states.
+               The darker the color, the higher the number of votes.
+               California has the highest votes. This map looks really
+               amazing with the added styles.")
+        ),
+        tags$a(href = "https://www.archives.gov/federal-register/electoral-college/about.html",
+               "for more information")
       )
     )
+  ),
+  tabPanel("other",
+           tags$h1("This page is completely empty")
   )
 
   # Create a tabPanel to show your scatter plot
